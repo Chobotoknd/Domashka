d721bf0453ed6f1a95790ead7f9f44316c856925
Chobotoknd <chobotoknd@gmail.com>
2022-09-05 23:27:00 +0400

    Статья с детьми детей

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-console.log('Hello, world!');
-let inputs = document.getElementsByTagName('*');
-console.log(inputs)
+const dict = {};
+
+function countRecursively(elements) {
+    Array.from(elements).forEach(function(element) {
+        if (dict[element.tagName.toLowerCase()] !== undefined) {
+            dict[element.tagName.toLowerCase()] += 1;
+        } else {
+            dict[element.tagName.toLowerCase()] = 1;
+        }
+        if (element.children.length > 0) {
+            countRecursively(element.children)
+        }
+    })
+}
+
+countRecursively(document.body.children)
+
+let numberOfTagsTemplate = "";
+
+Object.keys(dict).forEach((key) => {
+    let times = 'раза',
+        number = dict[key];
+    if ((number % 10) < 2 || (number % 10) > 4) {
+        times = 'раз'
+    }
+    numberOfTagsTemplate += `Тег <${key}> встречается ${number} ${times}\n`;
+});
+
+const numberOfTagsElement = document.createElement("div");
+
+numberOfTagsElement.innerText = numberOfTagsTemplate;
+
+document.body.appendChild(numberOfTagsElement);
